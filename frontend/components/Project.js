829decff49809703518b9395ec4c527de95d3328
Dumerat <hermelineyoan@gmail.com>
2023-04-23 20:55:41 +0200

    Project

--- frontend/components/Project.js
+++ frontend/components/Project.js
@@ -12,10 +12,20 @@ export default function Project() {
                         <div className='project-card-container' key={data.id}>
                             <div className='project-card' data-id={data.name}>
                                 <Image alt={data.name} src={data.image} width={360} height={250}/>
-                            </div>
-                            <div className='project-detail'>
-                                <h3>{data.name}</h3>
-                                <p>{data.desc}</p>
+                                <div className='project-detail'>
+                                    <div>
+                                        <h3>{data.name}</h3>
+                                        <p>{data.desc}</p>
+                                    </div>
+                                    <div className='project-link'>
+                                        {data.link ? (
+                                            <img src='/images/logo/link.svg' width={30} height={30} alt='Link' onClick={() => window.open(data.link)} />
+                                        ) : (
+                                            <img src='/images/logo/link.svg' width={30} height={30} alt='Link' style={{ opacity: '0' }} />
+                                        )}
+                                        <img src='/images/logo/github.svg' width={30} height={30} alt='Github' onClick={() => window.open(data.git)}/>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     )}
