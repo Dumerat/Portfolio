c6b0c66610e20dc569ff556a65fbf8abf3fc1175
Dumerat <hermelineyoan@gmail.com>
2023-04-26 02:57:33 +0200

    BACKUP

---
+++ frontend/Skill.d.ts
@@ -0,0 +1,6 @@
+type Skill = {
+    userId: number,
+    id: number,
+    title: string,
+    completed: boolean,
+}
--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -1,10 +1,18 @@
 import Styles from '@/styles/Footer.module.css'
+import Image from 'next/image'
 
 export default function Footer() {
 
     return(
         <footer className={Styles.footer}>
-            test
+            <div>
+                <Image src='/images/logo/github.svg' alt='github' width={30} height={30} onClick={() => window.open("https://github.com/Dumerat")}/>
+                <a href="mailto:dumeratonlaveur@gmail.com">
+                <Image src='/images/logo/mail.svg' alt='mail' width={30} height={30} onClick={() => "window.location.href='mailto:angel.uriot@gmail.com'"}/>
+                </a>
+                <Image src='/images/logo/linkedin.svg' alt='linkedIn' width={30} height={30} onClick={() => window.open("https://github.com/Dumerat")}/>
+            </div>
+                <h3>Yoan</h3>
         </footer>
     )
 }
